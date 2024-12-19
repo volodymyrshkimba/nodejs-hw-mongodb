c11063bb16b0b9b7ef23203d960cc293bb11b14c
Volodymyr Shkimba <shkimbavova@gmail.com>
2024-12-19 15:33:01 +0200

    do filter parser

---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,17 @@
+const parseContactType = (contactType) => {
+  const isString = typeof contactType === 'string';
+  if (!isString) return;
+
+  const isKnown = ['work', 'home', 'personal'].includes(contactType);
+  if (isKnown) return contactType;
+};
+
+const parseIsFavourite = (isFavourite) => {
+  const isString = typeof isFavourite === 'string';
+  if (!isString) return;
+
+  const isKnown = ['true', 'false'].includes(isFavourite);
+  if (!isKnown) return;
+
+  return isFavourite === 'true';
+};
