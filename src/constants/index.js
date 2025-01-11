623464fdfa134c4588d8fc49630546c22ef912b8
Volodymyr Shkimba <shkimbavova@gmail.com>
2025-01-11 18:55:14 +0200

    start do cloudinary

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import path from 'node:path';
+
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
 
@@ -8,3 +10,5 @@ export const SMTP = {
   SMTP_PASSWORD: 'SMTP_PASSWORD',
   SMTP_FROM: 'SMTP_FROM',
 };
+
+export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -28,6 +28,9 @@ const contactSchema = new Schema(
       required: true,
       ref: 'users',
     },
+    photo: {
+      type: String,
+    },
   },
   {
     timestamps: true,
